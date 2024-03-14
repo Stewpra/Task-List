5bd7d2902a1e47416ced3a10f36879ddf389abf0
Stewpra <wilbstewart7@gmail.com>
2024-03-13 22:37:31 -0400

    added modal. still need style and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,36 +3,49 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
-
-}
+function generateTaskId() {}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
-
-}
+function createTaskCard(task) {}
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
-
-}
+function renderTaskList() {}
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
-
-}
+function handleAddTask(event) {}
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
-}
+function handleDeleteTask(event) {}
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
-
-}
+function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+$(document).ready(function () {});
 
+$(document).ready(function () {
+  // Open the modal when the button is clicked
+  $("#openModalBtn").click(function () {
+    $("#taskModal").show();
+  });
+
+  // Close the modal when the close button is clicked
+  $(".close").click(function () {
+    $("#taskModal").hide();
+  });
+
+  // Handle form submission
+  $("#taskForm").submit(function (event) {
+    event.preventDefault();
+
+    // Get the form data
+    let title = $("#title").val();
+    let description = $("#description").val();
+    let deadline = $("#deadline").val();
+
+    // Add logic to save the task data (e.g., to localStorage or a server)
+
+    // Close the modal
+    $("#taskModal").hide();
+  });
 });
